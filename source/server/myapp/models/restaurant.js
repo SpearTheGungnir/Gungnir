b11182576658a9a385b27d88d7c55590c3923e8d
Godotcoffee <641276343@qq.com>
2016-07-07 15:33:37 +0800

    rm-rf

--- source/server/myapp/models/restaurant.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var userScheMa = new mongoose.Schema({
-  rname : String,
-  addr : String,
-  tel : String,
-  feature : String
-});
-module.exports = mongoose.model('restaurant', userScheMa);
--- source/server/myapp/models/user.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var userScheMa = new mongoose.Schema({
-    user: String,
-    pwd: String
-});
-module.exports = mongoose.model('user', userScheMa);
-
