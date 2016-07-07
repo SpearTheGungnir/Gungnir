b77c97cd81745fcd5f2b8d35d43ac8243c259423
louis <david11212014@outlook.com>
2016-07-07 09:21:38 +0800

    主界面初稿

---
+++ source/android/Gungnir/app/src/main/java/com/example/gj/gungnir/NearbyFragment.java
@@ -0,0 +1,17 @@
+package com.example.gj.gungnir;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by GJ on 2016/7/5.
+ */
+public class NearbyFragment extends Fragment{
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.nearbypage, container, false);
+    }
+}
