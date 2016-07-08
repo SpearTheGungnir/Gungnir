f10d15de90f9657e154a069e09a85113532d14ce
Godotcoffee <641276343@qq.com>
2016-07-08 23:56:15 +0800

    app.js

--- source/server/myapp/app.js
+++ source/server/myapp/app.js
@@ -3,10 +3,12 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var restaurants = require('./routes/restaurants');
 
 var app = express();
 
@@ -20,10 +22,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+	secret : '12345',
+	cookie : {maxAge : 3600000},
+	resave : false,
+	saveUninitialized : true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/restaurants', restaurants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
