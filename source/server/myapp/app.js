3c9e97646f65464aac5c4c6bb061770243540e9a
Godotcoffee <641276343@qq.com>
2016-07-10 16:52:48 +0800

    app.js

--- source/server/myapp/app.js
+++ source/server/myapp/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var restaurants = require('./routes/restaurants');
+var manager = require('./routes/manager');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/restaurants', restaurants);
+app.use('/manager', manager);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
