7822843d8fe17dbe40aa093aff327c817df26b4f
Godotcoffee <641276343@qq.com>
2016-07-13 09:29:21 +0800

    mysqlconnect

--- source/server/myapp/connect/mysql.js
+++ source/server/myapp/connect/mysql.js
@@ -1,12 +1,28 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host : '127.0.0.1',
-  user : 'gungnir',
-  password : '233',
-  database : 'gungnir',
-  port : '3306'
-});
+function handleError(err) {
+	if (err) {
+		connect();
+	} else {
+		console.error(new Date() + ': [handleError] - ' + err);
+	}
+}
+
+var connection;
+
+function connect() {
+	connection = mysql.createConnection({
+  	host : '127.0.0.1',
+  	user : 'gungnir',
+  	password : '233',
+  	database : 'gungnir',
+  	port : '3306'
+	});
+	connection.connect(handleError);
+	connection.on('error', handleError);
+}
+
+connect();
 
 module.exports = connection;
 
